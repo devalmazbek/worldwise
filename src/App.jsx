793d784c967cefb052459b922cd6b2c0f61ac999
devalmazbek <devalmazbek@gmail.com>
2024-02-09 18:58:33 +0600

    added button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/home";
 import Product from "./pages/product";
@@ -10,6 +10,7 @@ import AppLayout from "./pages/app-layout";
 import CityList from "./components/city-list";
 import CountryList from "./components/country-list";
 import City from "./components/city";
+import Form from "./components/form";
 
 const BASE_API = "http://localhost:8080";
 
@@ -42,10 +43,7 @@ function App() {
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/login" element={<Login />} />
           <Route path="/app" element={<AppLayout />}>
-            <Route
-              index
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
+            <Route index element={<Navigate replace to="cities" />} />
             <Route
               path="cities"
               element={<CityList cities={cities} isLoading={isLoading} />}
@@ -55,7 +53,7 @@ function App() {
               path="countries"
               element={<CountryList cities={cities} isLoading={isLoading} />}
             />
-            <Route path="form" element={<>form</>} />
+            <Route path="form" element={<Form />} />
           </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,18 +1,23 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 
 import styles from "./index.module.css";
 
 export default function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
 
+  const navigate = useNavigate();
+
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <h1>Map</h1>
       <p>Lat: {lat}</p>
       <p>Lng: {lng}</p>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 25 })}>
+        Change position
+      </button>
     </div>
   );
 }
